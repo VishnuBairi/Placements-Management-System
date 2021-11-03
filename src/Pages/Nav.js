eaf6dd2859bf9484ffea217079b23e8eb8d8aa4a
Nalla Ashok <ashoknalla99@gmail.com>
2021-11-04 00:48:14 +0530

    tird commit

--- src/Pages/Nav.js
+++ src/Pages/Nav.js
@@ -1,8 +1,13 @@
 import React from "react";
 function Nav() {
     return <div className='Navbar'>
-        <div><img  alt='a' src='Images/logo.png' style={{width:'50px'}}/></div>
+        <div className='logo'><img  alt='a' src='Images/logo.png' style={{width:'50px'}}/>
+        <div className="title">PLACEMENTS DRIVE</div>
+        </div>
+        
         <div className='inernav'>
+            <div className="log-in">Log In</div>
+            <div className="admin">Admin</div>
         </div>
     </div>
 }
